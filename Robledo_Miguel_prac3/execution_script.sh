8b1ca518c3034164d333a089d0519b74eb5ac77f
Miguel <mrobledo027@gmail.com>
2025-01-21 15:05:31 +0100

    faltaria implementar variables booleanes

--- Robledo_Miguel_prac3/execution_script.sh
+++ Robledo_Miguel_prac3/execution_script.sh
@@ -7,6 +7,6 @@ mkdir -p example_files/stdout_files
 input="example_files/input_files"
 output="example_files/output_files"
 stdout_output="example_files/stdout_files"
-for i in {1..17}; do
+for i in {1..19}; do
     ./bin/compiler $input/input_file$i.txt $output/output_file$i.txt > $stdout_output/stdout_file$i.txt
 done
